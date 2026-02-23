7ea9e0d66efd1392ef86e25f06adf443c5fcf20e
Abhinav Mudugu <am225mq@student.lnu.se>
2026-02-23 16:09:20 +0100

    Added logic to create /images and if it exists clean it. also fixed spliting into multiple different images by rendering

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use std::fs;
+use std::fs::{self, remove_dir_all, remove_file};
+use std::path::Path;
 use std::process::Command;
+use std::time;
 
 use rayon::prelude::*;
 
 fn main() {
+    let start_time = time::Instant::now();
+    let dir = "./data/images";
+
+    if !Path::new(dir).exists() {
+        fs::create_dir_all(dir).expect("failed to create dir");
+    } else{
+        fs::read_dir(dir).expect("failed to read images directory")
+        .map(|entry| entry.expect("failed to get dirEntry"))
+        .filter(|ent| ent.file_type().expect("unable to get file type").is_file())
+        .for_each(|ent| { fs::remove_file(ent.path()).expect("failed to remove file"); });
+    }
+
     let paths: Vec<_> = fs::read_dir("./data")
         .expect("failed to read data directory")
         .map(|ent| ent.expect("failed to get path"))
@@ -13,10 +27,13 @@ fn main() {
 
     paths.into_par_iter().for_each(|path| {
         let name = path.as_path().file_name().unwrap().display();
-        let _ = Command::new("pdfimages")
-            .arg("-j")
+        let _ = Command::new("pdftoppm")
+            .arg("-png")
+            .arg("-r").arg("200")
             .arg(path.as_os_str())
             .arg(format!("./data/images/{name}"))
-            .spawn();
+            .status().unwrap_or_else(|_| panic!("failed to convert to png {name}"));
     });
+    let elapsed = start_time.elapsed().as_secs();
+    println!("{elapsed}");
 }
